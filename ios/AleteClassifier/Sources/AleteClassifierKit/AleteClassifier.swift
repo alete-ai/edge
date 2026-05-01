bd5e0f2e93a942f101ae67ce92a6b33fe0edec9a
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2026-04-30 22:46:16 -0400

    update classifier

--- ios/AleteClassifier/Sources/AleteClassifierKit/AleteClassifier.swift
+++ ios/AleteClassifier/Sources/AleteClassifierKit/AleteClassifier.swift
@@ -4,9 +4,40 @@ public class AleteClassifier {
     private let engine: AleteClassifierEngine
     private let preprocessor: ContentPreprocessor
     
+    /// The bundle containing the model assets.
+    public static var bundle: Bundle {
+        #if SWIFT_PACKAGE
+        return Bundle.module
+        #else
+        return Bundle(for: AleteClassifier.self)
+        #endif
+    }
+
     public init(modelLoader: ModelLoader, tokenizer: AleteBertTokenizer) throws {
         self.engine = try AleteClassifierEngine(loader: modelLoader, tokenizer: tokenizer)
         self.preprocessor = ContentPreprocessor()
+    }
+
+    /**
+     * Factory method to load the default Model2Vec model from the package bundle.
+     */
+    public static func loadDefault() throws -> AleteClassifier {
+        let bundle = AleteClassifier.bundle
+        
+        guard let configURL = bundle.url(forResource: "m2v_head", withExtension: "json"),
+              let embeddingsURL = bundle.url(forResource: "m2v_embeddings", withExtension: "bin"),
+              let metaURL = bundle.url(forResource: "m2v_quant_meta", withExtension: "json"),
+              let tokenizerURL = bundle.url(forResource: "tokenizer", withExtension: "json") else {
+            throw ClassifierError.assetNotFound("Default model assets not found in bundle")
+        }
+        
+        let loader = try ModelLoader(configURL: configURL, embeddingsURL: embeddingsURL, metaURL: metaURL)
+        
+        let tokenizerData = try Data(contentsOf: tokenizerURL)
+        let tokenizerConfig = try JSONDecoder().decode(BertTokenizerConfig.self, from: tokenizerData)
+        let tokenizer = AleteBertTokenizer(config: tokenizerConfig)
+        
+        return try AleteClassifier(modelLoader: loader, tokenizer: tokenizer)
     }
     
     /**
